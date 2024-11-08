809f9d0b756618df3d2cddaf875204e2037fd9ee
Clyde Fare <clyde.fare@gmail.com>
2024-11-08 17:47:48 +0000

    including sam2 in prestart

--- pre_start.sh
+++ pre_start.sh
@@ -1,6 +1,12 @@
 cd /workspace;
 git clone https://github.com/xuebinqin/U-2-Net.git
+git clone https://github.com/facebookresearch/sam2.git
+cd sam2 && \
+	pip install -e ".[notebooks]"
+cd ..
+cd sam2/checkpoints && \
+	./download_ckpts.sh && \
+cd ../..
 mkdir /workspace/data;
 mkdir /workspace/data/raw_data;
-gdown https://drive.google.com/drive/folders/1cPtu2kBwzEn-OHPRG74WNePo6lyjVaZJ?usp=drive_link -O /workspace/data/raw_data;
 
